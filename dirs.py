e4a80b9d935f6f7ff90a25665cfbf94ba20cb530
Michał 'Khorne' Rzechonek <michal@rzechonek.net>
2015-02-02 21:24:03 +0100

    Asynchronous i/o

--- dirs.py
+++ dirs.py
@@ -20,14 +20,19 @@ class Mod:
 
 @asyncio.coroutine
 def load_mod(path):
+    @asyncio.coroutine
+    def read(file):
+        return file.read()
+
     sys.stdout.write("Loading mod from {} ".format(path))
     sys.stdout.flush()
     for i in range(0, 3):
         sys.stdout.write(".")
         sys.stdout.flush()
-        yield from asyncio.sleep(0.1)
-    sys.stdout.write(" ok\n")
-    return Mod(path)
+    with open(path) as f:
+        content = yield from read(f)
+        sys.stdout.write(" ok\n")
+        return content
 
 @asyncio.coroutine
 def load_all_mods(pattern):
